8ca89a80224f068b7286cde03b7dd28090eebcab
Joel Harrysson <joel94harrysson@gmail.com>
2021-11-05 14:27:28 +0100

    skapat klassen HomePage

---
+++ lib/home_page.dart
@@ -0,0 +1,33 @@
+//klassen HomePage hämtar TodoList
+//skapar sedan en floatingActionButton
+
+//Problem: Listan hamnar inte centralt fast jag använder Center-widget?
+//Kan inte använda mig av klassen Button.dart fastän jag kan skapa en egen knapp
+//direkt i home_page
+//TODO: Kolla detta på handledning
+
+import 'package:flutter/material.dart';
+import './todo_list.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Todo List'),
+        ),
+      ),
+      body: Center(
+        child: TodoList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('pressed');
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
