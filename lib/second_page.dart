8b2e88cdcc01ff591e307b20370fbd38b898def3
Joel Harrysson <joel94harrysson@gmail.com>
2021-11-15 20:40:23 +0100

    förenklar appBar + ny metod till knapp

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './button.dart';
+import 'package:my_first_app/todo_list_state.dart';
+import 'package:provider/provider.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen();
@@ -8,22 +9,18 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Container(
-          ///Placera Texten i mitten. Blev inte helt rätt med en Center-Widget
-          margin: EdgeInsets.only(left: 80),
-          child: Text('Enter Task'),
-        ),
+        title: Text('Enter Task'),
+
+        ///Placera Texten i mitten. Blev inte helt rätt med en Center-Widget
+        centerTitle: true,
       ),
       body: Column(
         children: [
           ///Hämtar metoden _inputText
           _inputText(),
+          _addButton(context)
 
           ///Ropar på Klassen Button med Namnargument för Icon och Text
-          Button(
-            buttonIcon: Icons.add_task,
-            buttonText: "Add task",
-          ),
         ],
       ),
     );
@@ -37,6 +34,22 @@ class SecondScreen extends StatelessWidget {
       child: TextField(
         decoration: InputDecoration(hintText: 'Task...'),
       ),
+    );
+  }
+
+  Widget _addButton(context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            var newTask = 'Test';
+            Provider.of<TodoListState>(context, listen: false)
+                .addListItem(newTask);
+          },
+          icon: Icon(Icons.add_task),
+        ),
+        Text('Add')
+      ],
     );
   }
 }
