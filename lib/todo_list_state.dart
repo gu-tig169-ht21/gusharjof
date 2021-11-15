b50d4850ee29d63e84daaeeb815ef49a6d3fc1bb
Joel Harrysson <joel94harrysson@gmail.com>
2021-11-15 21:39:45 +0100

    Metoder för att lägga till och ta bort ur listan

--- lib/todo_list_state.dart
+++ lib/todo_list_state.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class TodoListState extends ChangeNotifier {
   final List _list = [];
+  final String _item = '';
+  final int _index = 0;
 
   List get list => _list;
+  String get item => _item;
+  int get index => _index;
+
   void addListItem(item) {
     _list.add(item);
+    notifyListeners();
+  }
+
+  void removeListItem(index) {
+    _list.removeAt(index);
     notifyListeners();
   }
 }
