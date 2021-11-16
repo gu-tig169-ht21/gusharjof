dbdc9fca1b2ddaf20bb1de6752eaea02dbd4e13f
Joel Harrysson <joel94harrysson@gmail.com>
2021-11-16 08:32:07 +0100

    Tog bort överflödig getter

--- lib/todo_list_state.dart
+++ lib/todo_list_state.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 
 class TodoListState extends ChangeNotifier {
   final List _list = [];
-  final String _item = '';
-  final int _index = 0;
 
   List get list => _list;
-  String get item => _item;
-  int get index => _index;
 
   void addListItem(item) {
     _list.add(item);
