940fe2967eb337f4f40006d222fb2aca85d78d23
Joel Harrysson <joel94harrysson@gmail.com>
2021-11-17 11:55:50 +0100

    Lägger till Provider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './todo_list.dart';
 import './second_page.dart';
+import './todo_list_state.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -11,13 +13,16 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         actions: [
           PopupMenuButton(
+            icon: Icon(Icons.arrow_drop_down_circle_outlined),
+            offset: Offset(0, 45),
             onSelected: (value) {
-              print('Test');
+              Provider.of<TodoListState>(context, listen: false)
+                  .filterList(value);
             },
             itemBuilder: (context) => [
-              const PopupMenuItem(child: Text('All'), value: 1),
-              const PopupMenuItem(child: Text('Completed'), value: 2),
-              const PopupMenuItem(child: Text('Remaining'), value: 3)
+              const PopupMenuItem(child: Text('All'), value: 0),
+              const PopupMenuItem(child: Text('Completed'), value: 1),
+              const PopupMenuItem(child: Text('Remaining'), value: 2)
             ],
           ),
         ],
