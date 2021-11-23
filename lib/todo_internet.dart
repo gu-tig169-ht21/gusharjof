d9259b483bbf65b0c8d2d36582caa721f61d1b54
Joel Harrysson <joel94harrysson@gmail.com>
2021-11-23 09:26:06 +0100

    initial commit

---
+++ lib/todo_internet.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart' as http;
+
+class TodoInternet {
+  final String url =
+      'https://todoapp-api-pyq5q.ondigitalocean.app/todos?key=fad5ed99-c5eb-4167-9df7-dc564bcebc69';
+}
